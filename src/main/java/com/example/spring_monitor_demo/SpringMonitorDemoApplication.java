19bc540f269924b11205496e45279687c61d6142
oIoVinhGa <tpvinh292931@gmail.com>
2025-11-04 13:31:25 +0700

    init

---
+++ src/main/java/com/example/spring_monitor_demo/SpringMonitorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_monitor_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMonitorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMonitorDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_monitor_demo/SpringMonitorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_monitor_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMonitorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
